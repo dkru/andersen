f1551fca43e611d533e9a95b3567b34983f03fa9
Denys Krupenov <dkru84@gmail.com>
2021-04-29 20:34:13 +0300

    after refactoring

--- task-api/app/controllers/categories_controller.rb
+++ task-api/app/controllers/categories_controller.rb
@@ -3,15 +3,22 @@ class CategoriesController < ActionController::API
     category_id = params[:id]
     # Paste your code here
     category = Category.find(category_id)
-    render json: { category: {
+    render json: { category:
+                   {
                      name: category.name,
-                     products: category.products.map { |p| { 
-                       name: p.name,
-                       price: { value: p.price.value },
-                       images: p.images.map { |i| { id: i.id } }
-                     }}
-                   }
-                 }, status: :ok
+                     products: products_serialize(category)
+                   } }
+  end
+
+  private
+
+  def products_serialize(category)
+    category.products.map do |product|
+      {
+        name: product.name,
+        price: { value: product.price.value },
+        images: product.images.map { |i| { id: i.id } }
+      }
+    end
   end
 end
-
